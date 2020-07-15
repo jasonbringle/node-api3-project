15a9c384f1233d26a1a8fffac78f8d9daa8ffa2b
jasonbringle <jasonbringle@gmail.com>
2020-07-15 00:45:32 -0700

    Finished routes in userRouter.  Need to finish middleware in both userRouter and postRouter and middleware in postRouter

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const userRouter = require('./users/userRouter.js')
 
 const server = express();
+
+server.use(express.json());
+
+server.use(logger)
+
+server.use('/api/users', userRouter);
+
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +17,9 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} request`, `${req.url} request URL`, `${Date.now()} Current date`)
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,33 +1,80 @@
 const express = require('express');
 
+const UserDb = require('../users/userDb.js')
+const PostDb = require('../posts/postDb.js')
+
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  // do your magic!
+  const body = req.body;
+
+  UserDb.insert(body)
+    .then(user => 
+      res.status(200).json(user))
+    .catch(err =>
+      res.status(500).json({ error: "Error Adding User. Please Try Again."}))
 });
 
 router.post('/:id/posts', (req, res) => {
-  // do your magic!
+ const body = req.body;
+
+ UserDb.insert(body)
+  .then(post => 
+    res.status(201).json(post))
+  .catch(err =>
+    res.status(500).json({errormessage: 'Could not get posts.'}))
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  UserDb.get()
+  .then(users =>
+    res.status(201).json(users))
+  .catch(err =>
+    res.status(500).json({errormessage: 'Could not get users!'}))
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  const { id } = req.params;
+
+  UserDb.getById(id)
+    .then(user => 
+      res.status(200).json(user))
+    .catch(err => 
+      res.status(500).json({ errormessage: 'Could not get user'}))
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  const { id } = req.params
+  UserDb.getById(id)
+  .then(user => 
+    PostDb.getById(user.id)
+      .then(post =>
+        res.status(200).json(post))
+      .catch(err => 
+        res.status(500).json({ errormessage: 'Could not get post from that user'})))
+  .catch(err => 
+    res.status(500).json({ errormessage: 'Could not find that user.'}))
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  const { id } = req.params;
+
+  UserDb.remove(id)
+    .then(num => 
+      res.status(200).json(`${num} user has been eliminated`))
+    .catch(err =>
+      res.status(500).json({ errormessage: 'User has not been eliminated.'}))
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  const { id } = req.params;
+  const body = req.body;
+  
+  UserDb.update(id,body)
+    .then(edit =>
+      res.status(200).json(`${edit} user name was edited.`))
+    .catch(err =>
+      res.status(500).json({ errormessage: 'User was not edited.'}))
 });
 
 //custom middleware
