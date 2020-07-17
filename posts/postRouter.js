975a57f62169634357ab4f54e280c25278e00936
jasonbringle <jasonbringle@gmail.com>
2020-07-17 02:33:46 -0700

    Finished validation for postRouter...should be MVP and Stretch

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
-const PostDb = require('../posts/postDb.js')
+const PostDb = require('../posts/postDb.js');
+const UserDb = require('../users/userDb.js')
 
 
 const router = express.Router();
@@ -16,33 +17,35 @@ router.get('/', (req, res) => {
 router.get('/:id', validatePostId,(req, res) => {
   const { id } = req.params
   PostDb.getById(id)
-    .then(posts => {
-      if(posts.text.length > 0){
-      res.status(200).json(posts)}})
+    .then(post => {
+      if(post.text.length > 0){
+      res.status(200).json(post)}{
+        res.status(400).json({message: "No posts could be found."})
+      }})
     .catch(err => 
       res.status(500).json({ errormessage: "Could not find posts with that ID"}))
 });
 
 router.delete('/:id', validatePostId,(req, res) => {
   const { id } = req.params;
-  PostDb.getById(id)
-    .then(post =>{
-      if(post.id){
-        PostDb.remove(post.id)
+  PostDb.remove(id)
         .then(response => 
-          res.status(200).json({ message: `${response} post has been removed!`}))
+          {
+            if(response == 1){
+              res.status(200).json({ message: `${response} post has been removed!`})
+            }else{
+              res.status(400).json({ message: `The post is not found and is not removed!`})
+              }})
         .catch(err =>
-        res.status(500).json({ errormessage: 'The post could not be found and is not removed.'})
-        )} else { res.status(404).json({errormessage: "Could not find the post."})}
-      }
-        )
-    .catch(err =>
-      res.status(500).json({ errormessage:'Could not find that post.'}))
-});
+          res.status(500).json({ errormessage: 'There was an error and the post could not be found and is not removed.'})
+          )
+    }
+  );
 
-router.put('/:id', validatePostId,(req, res) => {
+router.put('/:id', validatePostId, validatePost,validateBody, validateUserId, (req, res) => {
   const { id } = req.params;
   const body = req.body;
+  console.log(body)
 
   PostDb.update(id, body)
     .then(num => 
@@ -54,15 +57,72 @@ router.put('/:id', validatePostId,(req, res) => {
 // custom middleware
 
 function validatePostId(req, res, next) {
- const { id } = req.params;
- PostDb.getById(id)
-  .then(post => {
-    if(post.id){
+  const { id } = req.params;
+    PostDb.getById(id)
+      .then(post =>{ 
+        if(post !== undefined){
+          next()
+        }else {
+          res.status(404).json({ message: "Post could not be located."})
+        }})
+      .catch(err =>
+      res.status(500).json({ errormessage: "Retrieving the post failed"}))
+}
+
+function validatePost(req, res, next) {
+  const { id } = req.params
+  const body = req.body
+  const keys = Object.keys(body)
+  console.log(body.id)
+  console.log(id)
+
+  if(body.id = id){if (keys.length == 0) {
+    res.status(400).json({ message: "missing post data" })
+  } else if (!body.text) {
+    res.status(400).json({ message: "missing required text field" })
+  } else {
+    next();
+  }
+  } else{
+    res.status(404).json({ message: 'User id in post and url do not match'})
+  }
+}
+
+
+function validateBody(req, res, next) {
+  const body = req.body
+  const keys = Object.keys(body)
+
+    if(keys.length == 0){ 
+      res.status(400).json({ message: "missing user data" })
+    }else if(!body.text){
+      res.status(400).json({ message: "missing required text field" } )
+    }{
       next();
     }
-  })
-  .catch(err =>
-    res.status(500).json({errormessage: 'It seems as though this post is not found!'}))
 }
+
+function validateUserId(req, res, next) {
+  const body = req.body
+  const userKey = body.user_id
+  
+    if(userKey){
+    UserDb.getById(body.user_id)
+      .then(user =>{
+        if(user){
+          next()
+        }else {
+          res.status(404).json({ errormessage: "The   user for this comment cannot be found."})
+        }
+      })
+  }else { 
+    res.status(400).json({ errormessage:" There is no user ID"})
+  }
+
+}
+
+
+
+
 
 module.exports = router;
